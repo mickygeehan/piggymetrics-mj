b73f461316d6b04b539c7624dce1806e760892fe
emicgee <michael.geehan@ericsson.com>
2018-10-09 16:47:49 +0100

    Finished connecting to db

--- account-service2/src/main/java/com/piggymetrics/account/controller/AccountServiceEndpoint.java
+++ account-service2/src/main/java/com/piggymetrics/account/controller/AccountServiceEndpoint.java
@@ -1,10 +1,22 @@
 package com.piggymetrics.account.controller;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.connection.ClusterSettings;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 
 @ApplicationScoped
 @Path("/")
@@ -14,6 +26,29 @@ public class AccountServiceEndpoint {
     @Path("/current")
     @Produces("text/plain")
     public Response getCurrentAccount() {
+        return Response.ok("Here is the current account!").build();
+    }
+
+
+
+    @GET
+    @Path("/test")
+    @Produces("text/plain")
+    public Response testDbConnection() {
+//        MongoClient mongoClient = MongoClients.create(
+//                MongoClientSettings.builder()
+//                        .applyToClusterSettings(builder ->
+//                                builder.hosts(Arrays.asList(new ServerAddress("172.18.0.6", 27018))))
+//                        .build());
+//        MongoDatabase db = mongoClient.getDatabase("piggymetrics");
+
+        MongoClient mongoClient = MongoClients.create(
+                new ConnectionString("mongodb://user:sd@account-mongodb/piggymetrics"));
+        MongoDatabase db = mongoClient.getDatabase("piggymetrics");
+        for (String name : db.listCollectionNames()) {
+            System.out.println(name);
+        }
+
         return Response.ok("Here is the current account!").build();
     }
 }
